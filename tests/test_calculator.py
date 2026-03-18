e7f7f4d133d998220a34ec6c261cf14414bf2343
zakaria <zakariaelouarassi@gmail.com>
2026-03-18 11:16:40 +0000

    Correction de la fonction subtract

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,7 @@
-from src.calculator import add,subtract
+from src.calculator import add, subtract
 
 def test_add(): 
     assert add(2, 3) == 5 
 
 def test_subtract():
     assert subtract(5, 2) == 3
-    
